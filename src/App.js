f2ec64364f82a4a254ea5d92cf487eb633bbc03d
mdsajedul <sajedulislms@gmail.com>
2021-09-20 18:40:11 +0600

    component contries created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-    
+    <Countries></Countries>
     </div>
   );
+}
+
+function Countries(){
+  const [countries,setCountries] = useState([]);
+  useEffect(()=>{
+   
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res=>res.json())
+    .then(data=>setCountries(data))
+  },[])
+  return(
+    <div>
+      <h2>Traveling around in the world</h2>
+      
+    </div>
+  )
 }
 
 export default App;
